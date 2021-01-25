e76866da9a3ce73f49c2f87b3b8108444da1a37d
Okechukwu Okigbo <okigbookey@gmail.com>
2021-01-25 16:07:07 -0500

    Update events.sol

--- level1/events.sol
+++ level1/events.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.7.0 <0.8.0;
 
 
 
-contract my2ndContract {
+contract my4thContract {
 
   // event declaration
   // events are way to communicate something that occured on the blockchain
