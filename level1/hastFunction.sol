433e40e99061bd9c65ba71da2eead445ffbbcf8f
Okechukwu Okigbo <okigbookey@gmail.com>
2021-01-25 15:28:52 -0500

    Update hastFunction.sol

--- level1/hastFunction.sol
+++ level1/hastFunction.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.8.0;
 
+
+contract my3rdContract {
+
+    struct customerAccount {
+        string name;
+        int balance;
+    }
+    
+    uint betta = 9050;
+    
+    function _randGenerator(string memory _str) private view returns(int) {
+        uint randnumber = uint(keccak256(abi.encodePacked(_str)));
+        return randnumber % betta;
+      
+    }
+    
+    
+}
