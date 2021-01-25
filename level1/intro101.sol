7aaf6cf8e76328d49a16f6ca9512e5d3261d5354
Okechukwu Okigbo <okigbookey@gmail.com>
2021-01-25 10:52:33 -0500

    Update intro101.sol

--- level1/intro101.sol
+++ level1/intro101.sol
@@ -4,6 +4,16 @@ pragma solidity >=0.7.0 <0.8.0;
 
 contract myFirstContract {
 
-    //start here
+    // state variable and numeric operators
+    // state variables are permanently stored on the Ethereum blockchain
+    
+    // numeric types are int and unit
+    // uint => unsigned integers: only positive number
+    // int => normal integres: both positive and negative numbers
+    // uint usually defaults to uint256,
+    // others are uint8, uint16, uint36
+    
+    uint firstMoney = 160000000;
+    
 
 }
