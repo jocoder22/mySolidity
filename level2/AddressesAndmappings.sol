f010f57471dd0157a3b5bfb5c5c79b10b6dfca15
Okechukwu Okigbo <okigbookey@gmail.com>
2021-01-26 14:44:17 -0500

    Update AddressesAndmappings.sol

--- level2/AddressesAndmappings.sol
+++ level2/AddressesAndmappings.sol
@@ -1,11 +1,12 @@
 pragma solidity >=0.7.0 <0.8.0;
 
-
-
 contract my5thContract {
 
     struct customerAccount {
         string name;
         int balance;
     }
+    
+    // Address is the account identifier for Ethereum users
+    // mapping is key-value method for data storage and look-up
 }
