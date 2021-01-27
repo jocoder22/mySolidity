c0bd34c37ce1d8c322a40a6a7ea9b6641f887998
Okechukwu Okigbo <okigbookey@gmail.com>
2021-01-27 11:29:52 -0500

    Update mgsSender.sol

--- level2/mgsSender.sol
+++ level2/mgsSender.sol
@@ -1,3 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.8.0;
 
-// msg.sender is the address of person or smart contract
-// that calls a function in a particular smart contract
+contract my6thContract {
+
+   mapping (uint => address) addressOwner;
+   mapping (address => uint) cytoBalance;
+
+    // msg.sender is the address of person or smart contract
+    // that calls a function in a particular smart contract
+    // can it in mapping, functions etc
+    
+    function _showOwner(unit _id, uint _amount) private view returns (string) {
+        
+        // Assign current Id to calling address
+        addressOwner[_id] = msg.sender;
+        
+        // store the _amount under the calling address
+        cytoBalance[msg.sender] = _amount
+        
+        
+        return cytoBalance[msg.sender];
+        
+    }
+
+
+}
