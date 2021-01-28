3fc4b4f2a36d20d33891ad3669891a0498abe335
Okechukwu Okigbo <okigbookey@gmail.com>
2021-01-28 10:54:44 -0500

    Update require.sol

--- level2/require.sol
+++ level2/require.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.8.0;
 
+contract my6thContractUpdate {
+
+   // require will throw an error and stop the function if
+   // the condition is false
+
+   mapping (uint => address) addressOwner;
+   mapping (address => uint) cytoBalance;
+
+    // msg.sender is the address of person or smart contract
+    // that calls a function in a particular smart contract
+    
+    function _showOwner(unit _id, uint _amount) private view returns (string memory) {
+        // condition must be true for the function to run
+        // here the cytobalance of the caller must be at least 30
+        require(cytoBalance[msg.sender] >= 30);
+        
+        // Assign current Id to calling address
+        addressOwner[_id] = msg.sender;
+        
+        // store the _amount under the calling address
+        cytoBalance[msg.sender] = _amount
+        
+        // increase the _amount under the calling address by 1
+        cytoBalance[msg.sender]++;
+        
+        // return or retrieve the amount store for the caller
+        return cytoBalance[msg.sender];
+        
+    }
+
+
+}
